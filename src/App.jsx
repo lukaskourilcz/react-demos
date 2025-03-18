febbb4e92aecc3fa3ff5825c4cef457b7d9e11e6
Lukas Kouril <kouril.lukas@gmail.com>
2025-03-18 10:41:40 +0700

    use-state-2

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,22 @@ import { useState } from "react";
 
 function App() {
 
-  const [friends, setFriends] = useState(['Alex', 'Jones'])
-  const addOneFriend = () => setFriends([...friends, 'Lukas', 'Kakoak'])
-  const removeOneFriend = () => setFriends(friends.filter((f) => f !== "Jones" ))
-  const updateOneFriend = () => {
-    setFriends(friends.map(f => f === 'Alex' ? 'Alan' : f))
-  }
+ const [movies, setMovies] = useState([
+    {id: 1, title: 'Spider Man', ratings: 3},
+    {id: 2, title: 'Spider Man 2', ratings: 6}
+  ])
 
+const handleClick = () => {
+  setMovies(movies.map(m => m.id === 1 ? {...movies, title: "John Wick"} : m))
+}
 
   return (
     <section>
-      {friends.map((f) => (
-        <li key={Math.random()}>{f}</li>
-      ))}
-      <button onClick={addOneFriend}>Add New Friend</button>
-      <button onClick={removeOneFriend}>Remove One Friend</button>
-      <button onClick={updateOneFriend}>Update One Friend</button>
 
+      {movies.map(m => (
+        <li key={Math.random()}>{m.title}</li>
+      ))}
+      <button onClick={handleClick}>Change Movie</button>
     </section>
   );
 }
